7eca4d55e55c27ac377cfad7a4821ccdb5acc123
FayadAl-Habsyi <fayedalhabsyi636@gmail.com>
2021-04-03 12:31:48 +0800

    Menambahkan SUM4

--- MyClass.java
+++ MyClass.java
@@ -3,8 +3,10 @@ public class MyClass {
     int sum1 = 100 + 50;
     int sum2 = sum1 + 250;
     int sum3 = sum2 + sum2;
+    int sum4 = 200 + 500;
     System.out.println(sum1);
     System.out.println(sum2);
     System.out.println(sum3); 
+    System.out.println(sum4); 
   }
 }
